10d192013e8ace1ef3a0cb473dabc7f5619f9862
KarpenyaT <karpenya.tatyana@mail.ru>
2024-03-05 13:09:05 +0200

    Change to RecyclerView and ListAdapter

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,13 +1,13 @@
 package ru.netology.nmedia.dto
 
 data class Post (
-    val id:Int,
-    val author:String,
-    val published:String,
-    val content:String,
-    val likes:Int=0,
-    val likedByMe:Boolean=false,
-    val shareCount:Int=0,
-    val shareByMe:Boolean=false,
-    val viewCount:Int=0
+    val id: Long,
+    val author: String,
+    val published: String,
+    val content: String,
+    val likes: Long = 0,
+    val likedByMe: Boolean = false,
+    val shareCount: Long = 0,
+    val shareByMe: Boolean = false,
+    val viewCount: Long = 0
 )
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,8 +1,8 @@
 import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
-interface PostRepository{
-    fun get(): LiveData<Post>
-    fun like()
-    fun share()
+interface PostRepository {
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun shareById(id: Long)
 }
