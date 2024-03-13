29b561c01120d6d95771c14ee92803628a876cae
KarpenyaT <karpenya.tatyana@mail.ru>
2024-03-13 21:04:08 +0200

    Change imageView to MaterialButton

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post (
     val author: String,
     val published: String,
     val content: String,
-    val likes: Long = 0,
+    val likeCount: Long = 0,
     val likedByMe: Boolean = false,
     val shareCount: Long = 0,
     val shareByMe: Boolean = false,
